d90fd6453555c82d9e3af8d3e0385f8fb2dd0120
Andrew Stearns <andrew.stearns@sciencespo.fr>
2019-06-06 11:43:32 +0200

    Listing api crud

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,15 +3,15 @@ class Listing < ApplicationRecord
   has_many :reservations, dependent: :destroy
   has_many :missions, dependent: :destroy
 
-  validates :room_num, presence: true, numericality: { only_integer: true }
+  validates :num_rooms, presence: true, numericality: { only_integer: true }
 
   validate :greater_than_zero?
 
   private
 
   def greater_than_zero?
-    if self.room_num < 1
-      self.errors.add(:room_num, "Listing must have at least one room.")
+    if self.num_rooms < 1
+      self.errors.add(:num_rooms, "Listing must have at least one room.")
     end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Listing.destroy_all
+
+listing_1 = Listing.new(num_rooms: 2)
+listing_1.save
+
+listing_2 = Listing.new(num_rooms: 3)
+listing_2.save
+
+listing_3 = Listing.new(num_rooms: 5)
+listing_3.save
+
+puts "3 listings created"
