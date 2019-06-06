5ae87e5076c2518d50dbf6460cc0296edd823916
Andrew Stearns <andrew.stearns@sciencespo.fr>
2019-06-06 12:48:38 +0200

    Reservations are validated for booking window and other reservation conflicts

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -5,12 +5,38 @@ class Reservation < ApplicationRecord
   validates :end_date, presence: true
 
   validate :start_before_end?
+  validate :in_booking_timeframe?
+  validate :no_reservation_conflict?
 
   private
 
   def start_before_end?
     if self.start_date >= self.end_date
       self.errors.add(:start_date, "Start date must before end_date.")
+    end
+  end
+
+  def in_booking_timeframe?
+    within_timeframe = false
+    self.listing.bookings.each do |booking|
+      if booking.start_date <= self.start_date && booking.end_date >= self.end_date
+        within_timeframe = true
+      end
+    end
+    if !within_timeframe
+      self.errors.add(:start_date, "No reservation for unavailable apartment")
+    end
+  end
+
+  def no_reservation_conflict?
+    reservation_conflict = false
+    self.listing.reservations.each do |reservation|
+      if self.start_date >= reservation.start_date && self.start_date <= reservation.end_date
+        reservation_conflict = true
+      end
+    end
+    if reservation_conflict
+      self.errors.add(:start_date, "There is another reservation for these dates.")
     end
   end
 end
