378323f36d3321ea931e45cae67696678f0332bf
Andrew Stearns <andrew.stearns@sciencespo.fr>
2019-06-06 11:06:29 +0200

    Routes drawn and homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  get 'pages/home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "pages#home"
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :listings, only: [:index, :show, :create, :update, :destroy]
+      resources :bookings, only: [:index, :show, :create, :update, :destroy]
+      resources :reservations, only: [:index, :show, :create, :update, :destroy]
+    end
+  end
+
 end
